a543465bd6f0dfa6db7019fbbcd3d7fafc383158
xinrayjilong <xinrayjilong@gmail.com>
2025-08-19 14:28:51 +0800

    生成报告接口配置文件修改

--- generated_backend/config.py
+++ generated_backend/config.py
@@ -87,8 +87,8 @@ class Config:
     RAG_API_KEY = os.environ.get('RAG_API_KEY', 'ragflow-VmMWVkNGUwNjhmYTExZjBhNTgzNzYwNT')
 
     # 报告生成API配置
-    REPORT_API_URL = os.environ.get('REPORT_API_URL', 'http://172.16.18.157:18080/v1/workflows/run')
-    REPORT_API_KEY = os.environ.get('REPORT_API_KEY', 'app-TPm1UnL2LeXaMnZI4c03frYN')
+    REPORT_API_URL = os.environ.get('REPORT_API_URL', 'http://172.16.76.203/v1/chat-messages')
+    REPORT_API_KEY = os.environ.get('REPORT_API_KEY', 'app-c8cKydhESsFxtG7QZvZkR5YU')
 
     # 文档处理服务API配置
     DOCUMENT_PROCESS_API_URL = os.environ.get('DOCUMENT_PROCESS_API_URL', 'http://localhost:7860/api/process')
